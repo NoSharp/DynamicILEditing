0a8e078e0fac482f6faf8dfe142f7fe3f128039a
Harry Kerr <33969918+NoSharp@users.noreply.github.com>
2020-02-07 11:27:58 +0000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using System;
+
+namespace DynamicILEdit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir = "mystartDir";
+
+            ModuleDefinition module = ModuleDefinition.ReadModule(dir + "\\ClassLibrary1.dll");
+            foreach (TypeDefinition type in module.Types)
+            { 
+                foreach (var meth in type.Methods) {
+                    var name = meth.Name;
+                    if (name == "Main")
+                    {
+                        //1: LDSTR
+                        var body = meth.Body;
+                        var processor = body.GetILProcessor();
+                        var createInstruction = processor.Create(OpCodes.Ldstr, "Hello World");
+                        processor.Replace(body.Instructions[1], createInstruction);
+                    }
+                }
+                Console.WriteLine(type.FullName);
+            }
+
+            foreach (TypeDefinition type in module.Types)
+            {
+                foreach (var meth in type.Methods)
+                {
+                    var name = meth.Name;
+                    if (name == "Main")
+                    {
+                        //1: LDSTR
+                        var body = meth.Body;
+                        foreach (var i in body.Instructions) {
+                            Console.WriteLine(i.ToString());
+                        }
+                    }
+                }
+                Console.WriteLine(type.FullName);
+            }
+            module.Write(dir + "\\ClassLibrary2.dll");
+            Console.ReadKey();
+
+        }
+    }
+}
